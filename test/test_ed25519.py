03e1fcc33c17515fb1aa1f21f8ebdcfbc0e3e010
Maximilian <23641084+xfallme@users.noreply.github.com>
2025-12-03 16:02:44 +0100

    test for ed25519ph, and rfc8032 test vectors for normal ed25519

--- test/test_ed25519.py
+++ test/test_ed25519.py
@@ -2,11 +2,12 @@ import binascii
 import pytest
 from eddsa.algorithms.ed25519 import Ed25519
 from eddsa.algorithms.ed25519ctx import Ed25519CTX
+from eddsa.algorithms.ed25519ph import Ed25519PH
 from eddsa.keys.ed25519_keypair import Ed25519Keypair
 from test_cases_eddsa_vectors import *
 
 
-@pytest.mark.parametrize("lines", [case_ed25519_test_vectors_cr_yp_to()], ids=["Ed25519 Test Vectors from cr.yp.to"])
+@pytest.mark.parametrize("lines", [case_ed25519_test_vectors_cr_yp_to(), case_ed25519_test_vectors_rfc8032()], ids=["Ed25519 cr.yp.to", "Ed25519 RFC8032"])
 def test_ed25519_signature(lines, request):
     print(f"Running with {len(lines)} inputs for test '{request.node.callspec.id}'")
 
@@ -28,6 +29,7 @@ def test_ed25519_signature(lines, request):
         is_valid = Ed25519.verify(s, m, keypair.public_bytes())
         assert is_valid, "Signature verification failed"
 
+
 @pytest.mark.parametrize("lines", [case_ed25519ctx_test_vectors_rfc8032()], ids=["Ed25519CTX Test Vectors from RFC8032"])
 def test_ed25519ctx_signature(lines, request):
     print(f"Running with {len(lines)} inputs for test '{request.node.callspec.id}'")
@@ -39,7 +41,7 @@ def test_ed25519ctx_signature(lines, request):
         keypair = Ed25519Keypair.from_private_bytes(sk)
         pk = binascii.unhexlify(x[1])
         assert keypair.public_bytes() == pk, "Derived public key does not match expected public key"
-        
+
         m = binascii.unhexlify(x[2])
         ctx = binascii.unhexlify(x[3])
         s = binascii.unhexlify(x[4])
@@ -50,4 +52,29 @@ def test_ed25519ctx_signature(lines, request):
 
         # Verify the signature
         is_valid = Ed25519CTX.verify(s, m, keypair.public_bytes(), ctx)
+        assert is_valid, "Signature verification failed"
+
+
+@pytest.mark.parametrize("lines", [case_ed25519ph_test_vectors_rfc8032()], ids=["Ed25519PH Test Vectors from RFC8032"])
+def test_ed25519ph_signature(lines, request):
+    print(f"Running with {len(lines)} inputs for test '{request.node.callspec.id}'")
+
+    for line in lines:
+        x = line.split(':')
+
+        sk = binascii.unhexlify(x[0])
+        keypair = Ed25519Keypair.from_private_bytes(sk)
+        pk = binascii.unhexlify(x[1])
+        assert keypair.public_bytes() == pk, "Derived public key does not match expected public key"
+
+        m = binascii.unhexlify(x[2])
+        ctx = binascii.unhexlify(x[3])
+        s = binascii.unhexlify(x[4])
+
+        # Sign the message
+        generated_s = Ed25519PH.sign(m, keypair, ctx)
+        assert generated_s == s, "Generated signature does not match expected signature"
+
+        # Verify the signature
+        is_valid = Ed25519PH.verify(s, m, keypair.public_bytes(), ctx)
         assert is_valid, "Signature verification failed"
