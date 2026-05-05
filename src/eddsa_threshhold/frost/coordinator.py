e0a02a9aa0d606285a855031a7e13801935e6f56
Maximilian <23641084+xfallme@users.noreply.github.com>
2026-05-05 16:07:22 +0200

    refactor

--- src/eddsa_threshhold/frost/coordinator.py
+++ src/eddsa_threshhold/frost/coordinator.py
@@ -1,5 +1,3 @@
-from typing import Any, Tuple
-
 from eddsa_threshhold.eddsa.curves.base.edwards_curve import EdwardsCurve
 from eddsa_threshhold.frost.core.base.frost_hashing import FrostHashing
 from eddsa_threshhold.frost.core.secrets.shamir_secret_sharing import ShamirSecretSharing
